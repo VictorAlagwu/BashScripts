f7f9063747100325fb90a4ada734c2808c5f17ca
VictorAlagwu <victoralagwu@gmail.com>
2018-09-05 18:44:41 +0100

    Param sh

---
+++ param.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "The name of the program is: $0"
+echo "The first parameter is: $1 "
+echo "The second parameter is: $2 "
+echo "The third parameter is: $3 "
+echo "Printing every parameter: $*"
+echo 
+echo "Thanks for checking out $0 "
