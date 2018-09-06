cf3b3c05a03ff98979e00b5326c3f06dba3473e8
VictorAlagwu <victoralagwu@gmail.com>
2018-09-06 10:12:04 +0100

    Updating shell

--- dirCreator.sh
+++ dirCreator.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
-
 # Ask User for a new directory name
-
 echo "Enter the name for the new directory to be created: "
 read -r dir
-mkdir "$dir" ; cd "$dir" || return; pwd ; 
-touch victor.txt; touch test.txt; 
-ls ; echo "My name is Victor Alagwu" >> victor.txt; echo "Checking test file" >> test.txt; 
-cat victor.txt; 
+
+#Save Old Directory
+ORIG_DIR=$(pwd)
+# Make the directory
+mkdir "$dir"
+
+# Entering the directory 
+cd "$dir" || return
+
+# Checking the directory link
+pwd 
+
+# Creating new files
+for n in 1 2 3 4 5
+do
+	touch victorfile$n
+done
+
+# Checking directory
+ls victorfile?
+
+# Adding Contents to the files
+for contents in victorfile?
+do 
+	echo The content added are $contents > $contents
+done
+
+#Show contents of the files
+
+cat victorfile? 
+
+# Say GoodBye
+cd $ORIG_DIR
+rm -rf "$dir"
 echo "GoodBye Victor";
-cd ..; rm -rf "$dir";
